e44d35099788185fa7f050c86daa29ac2cdbc389
Deep1704 <95118819+Deep1704@users.noreply.github.com>
2022-03-24 13:45:07 +0530

    Add files via upload

---
+++ Myprog.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Myprog
+{
+public static void main(String[] args) {
+
+	Scanner input=new Scanner(System.in);
+	System.out.println("Enter your Name:");
+	String name=input.nextLine();
+
+	System.out.println("Enter your City:");
+	String city=input.nextLine();
+	
+	System.out.println("Enter your Class:");
+	String classb=input.nextLine();
+
+	System.out.println("");
+
+
+	System.out.println("Name:"+name); 
+	System.out.println("City:"+city); 
+	System.out.println("Class:"+classb);
+	
+	}
+}	
